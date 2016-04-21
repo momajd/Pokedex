0f4d7584990fe4abb8d810604fd1d8745834e30a
momajd <mo.a.majd@gmail.com>
2016-04-21 10:51:46 -0700

    Phase 1 complete

---
+++ frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<div>Content Loaded</div>, document.getElementById('root'));
+});
